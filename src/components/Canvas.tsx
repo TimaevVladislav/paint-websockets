4bb228f592f1058ee7b35ef4b4c2ae30ea692173
timaevV <timaevvladislav@gmail.com>
2023-10-22 20:07:36 +0600

    add:canvas reference <useRef>

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,12 +1,21 @@
-import React from "react"
+import React, {LegacyRef, MutableRefObject, useEffect, useRef} from "react"
+import {observer} from "mobx-react-lite"
 import "../css/modules/Canvas.module.scss"
+import canvas from "../store/canvas"
 
-const Canvas = () => {
+const Canvas = observer(() => {
+
+    const reference = useRef() as MutableRefObject<HTMLCanvasElement>
+
+    useEffect(() => {
+        canvas.setCanvas("canvas")
+    }, [])
+
     return (
         <div className="canvas">
-          <canvas width={600} height={400} />
+          <canvas ref={reference} width={600} height={400} />
         </div>
     )
-}
+})
 
 export default Canvas
