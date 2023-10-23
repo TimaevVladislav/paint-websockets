001f0473718cdcd21655bc07a96fd8b1033b9fd8
timaevV <timaevvladislav@gmail.com>
2023-10-24 04:03:57 +0600

    add:set tool to store

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -1,20 +1,56 @@
 import React from "react"
 import "../css/modules/Toolbar.module.scss"
+import tools from "../store/tools"
+import canvas from "../store/canvas"
+
+import Brush from "../tools/Brush"
 
 const Toolbar = () => {
+
     return (
         <div className="toolbar">
-          <button className="toolbar__btn brush"></button>
+            <button
+                className="toolbar__btn brush"
+                onClick={() => tools.setTool({tool: new Brush(canvas.canvas)})}
+            />
 
-            <button className="toolbar__btn rect"></button>
-            <button className="toolbar__btn circle"></button>
-            <button className="toolbar__btn eraser"></button>
-            <button className="toolbar__btn line"></button>
+            <button
+
+                className="toolbar__btn rect"
+                onClick={() => tools.setTool({tool: new Brush(canvas.canvas)})}
+            />
+
+            <button
+                className="toolbar__btn circle"
+                onClick={() => tools.setTool({tool: new Brush(canvas.canvas)})}
+            />
+
+            <button
+                className="toolbar__btn eraser"
+                onClick={() => tools.setTool({tool: new Brush(canvas.canvas)})}
+            />
+
+            <button
+                className="toolbar__btn line"
+                onClick={() => tools.setTool({tool: new Brush(canvas.canvas)})}
+            />
 
             <input type="color" />
-            <button className="toolbar__btn undo"></button>
-            <button className="toolbar__btn redo"></button>
-            <button className="toolbar__btn save"></button>
+
+            <button
+                className="toolbar__btn undo"
+                onClick={() => tools.setTool({tool: new Brush(canvas.canvas)})}
+            />
+
+            <button
+                className="toolbar__btn redo"
+                onClick={() => tools.setTool({tool: new Brush(canvas.canvas)})}
+            />
+
+            <button
+                className="toolbar__btn save"
+                onClick={() => tools.setTool({tool: new Brush(canvas.canvas)})}
+            />
         </div>
     )
 }
