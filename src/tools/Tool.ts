e1d7852fe042f3f4fb598a8a3e908af75c5e0644
timaevV <timaevvladislav@gmail.com>
2023-10-27 03:58:11 +0600

    add:setters tool setting

--- src/tools/Tool.ts
+++ src/tools/Tool.ts
@@ -9,6 +9,18 @@ export default class Tool {
         this.destroyEvents()
     }
 
+    set fillColor(color: string) {
+        this.ctx!.fillStyle = color
+    }
+
+    set strokeColor(color: string) {
+       this.ctx!.strokeStyle = color
+    }
+
+    set lineWidth(width: number) {
+      this.ctx!.lineWidth = width
+    }
+
     destroyEvents(): void {
         this.canvas.onmousemove = null
         this.canvas.onmousedown = null
